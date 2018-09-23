b28d2b58e6d419ee676aa2c2f7b6bdf38d0234ba
wutong19 <wutong19@baidu.com>
2018-09-23 17:17:18 +0800

    come back

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,26 +1,26 @@
 import _ from 'lodash';
-import './style.css';
-import './style2.css';
-import demo from './asset/images/demo.jpeg';
-import demo2 from './asset/images/demo2.jpg';
+// import './style.css';
+// import './style2.css';
+// import demo from './asset/images/demo.jpeg';
+// import demo2 from './asset/images/demo2.jpg';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.className = 'hello';
+  // element.className = 'hello';
 
   return element;
 }
 
 document.body.appendChild(component());
-
-var img = new Image();
-img.src = demo;
-
-var img2 = new Image();
-img2.src = demo2;
-
-document.body.appendChild(img);
-document.body.appendChild(img2);
+//
+// var img = new Image();
+// img.src = demo;
+//
+// var img2 = new Image();
+// img2.src = demo2;
+//
+// document.body.appendChild(img);
+// document.body.appendChild(img2);
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -9,42 +9,42 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000
-            }
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 65
-              }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
+    // rules: [
+    //   {
+    //     test: /\.css$/,
+    //     use: [
+    //       MiniCssExtractPlugin.loader,
+    //
+    //       'css-loader'
+    //     ]
+    //   },
+    //   {
+    //     test: /\.(png|jpg|jpeg|gif)$/,
+    //     use: [
+    //       {
+    //         loader: 'url-loader',
+    //         options: {
+    //           limit: 10000
+    //         }
+    //       },
+    //       {
+    //         loader: 'image-webpack-loader',
+    //         options: {
+    //           mozjpeg: {
+    //             progressive: true,
+    //             quality: 65
+    //           }
+    //         }
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     test: /\.(woff|woff2|eot|ttf|otf)$/,
+    //     use: [
+    //       'file-loader'
+    //     ]
+    //   }
+    // ]
   },
   plugins: [
     new MiniCssExtractPlugin({
