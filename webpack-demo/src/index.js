9cfc8806555555482e0279f3891414eaae444f32
wutong19 <wutong19@baidu.com>
2018-09-23 16:31:35 +0800

    add extract css

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import './style2.css';
+import imgdemo from './asset/images/demo.jpeg';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.className = 'hello';
 
   return element;
 }
 
 document.body.appendChild(component());
+
+var img = new Image();
+img.src = imgdemo;
+
+document.body.appendChild(img);
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,34 @@
 const path = require('path');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ]
 }
