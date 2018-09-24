397d523947d05e80332a4c0215a897c9236d949f
wutong19 <wutong19@baidu.com>
2018-09-24 11:39:21 +0800

    tree shaking

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,30 +1,13 @@
-import _ from 'lodash';
-import printMe from './print';
+import {cube} from './math';
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.onclick = printMe;
-  btn.innerHTML = 'Click me and check the console!';
-
-  element.appendChild(btn);
+function app() {
+  var element = document.createElement('pre');
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
 
-let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
-document.body.appendChild(element);
-
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    printMe();
-
-    document.body.removeChild(element);
-    element = component(); // 重新渲染页面后，component 更新 click 事件处理
-    document.body.appendChild(element);
-  });
-}
+document.body.appendChild(app());
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,32 +1,33 @@
 const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const WebpackManifestPlugin = require('webpack-manifest-plugin');
-const webpack = require('webpack');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const WebpackManifestPlugin = require('webpack-manifest-plugin');
+// const webpack = require('webpack');
 
 module.exports = {
+  mode: 'production',
   entry: {
     app: './src/index.js'
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    // publicPath: '/'
   },
-  devtool: 'cheap-source-map',
+  // devtool: 'cheap-source-map',
   module: {
   },
-  devServer: {
-    contentBase: 'dist',
-    hot: true
-  },
+  // devServer: {
+  //   contentBase: 'dist',
+  //   hot: true
+  // },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new WebpackManifestPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
-    new webpack.HotModuleReplacementPlugin()
+    // new CleanWebpackPlugin(['dist']),
+    // new WebpackManifestPlugin(),
+    // new HtmlWebpackPlugin({
+    //   title: 'Output Management'
+    // }),
+    // new webpack.HotModuleReplacementPlugin()
   ]
 }
